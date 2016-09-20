4cf0290c9fed761fa0d2c1b32e84c225d676ef2e
Miguel Angel Duran Garcia <miguelangel.duran@scmspain.com>
2016-09-20 08:20:12 +0200

    Remove from npm package not needed files and fix webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,10 @@
-var path = require('path');
-var webpack = require('webpack');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var merge = require('webpack-merge');
+const path = require('path')
+const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const merge = require('webpack-merge')
 
-var APP_PATH = path.join(__dirname, '/src');
-var TARGET = process.env.npm_lifecycle_event;
-
-var path = require('path');
-var webpack = require('webpack');
+const APP_PATH = path.join(__dirname, '/src')
+const TARGET = process.env.npm_lifecycle_event
 
 var base = {
   resolve: {
@@ -22,18 +19,18 @@ var base = {
       test: /\.s?css$/,
       loader: ExtractTextPlugin.extract('css!sass')
     }, {
-        test: /\.svg$/,
-        loader: 'svg-inline'
+      test: /\.svg$/,
+      loader: 'svg-inline'
     }]
   },
   plugins: [
     new webpack.DefinePlugin({
       'process.env': {
         'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-       }
+      }
     })
   ]
-};
+}
 
 if (TARGET === 'start:server' || !TARGET) {
   module.exports = merge(base, {
@@ -59,29 +56,5 @@ if (TARGET === 'start:server' || !TARGET) {
         allChunks: true
       })
     ]
-  });
-}
-
-
-if (TARGET === 'dist') {
-  module.exports = merge(base, {
-    entry: {
-      app: path.resolve(__dirname, 'docs/index.jsx'),
-      vendors: ['react', 'react-dom', 'babel-polyfill']
-    },
-    output: {
-      filename: 'dist/index.[hash].js'
-    },
-    plugins: [
-      new webpack.optimize.UglifyJsPlugin({
-        compress: {
-          warnings: false
-        }
-      }),
-      new ExtractTextPlugin('dist/index.[contenthash].css', {
-        allChunks: true
-      }),
-      new webpack.optimize.CommonsChunkPlugin('vendors', 'dist/vendors.[hash].js')
-    ]
-  });
+  })
 }
