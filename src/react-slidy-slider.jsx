f560d7ad82c53d7a9b85b22b654fb1441725a696
Miguel Angel Duran Garcia <miguelangel.duran@scmspain.com>
2016-09-28 14:34:14 +0200

    Hot fix in order to check the destroy slider before unmounting it

--- src/react-slidy-slider.jsx
+++ src/react-slidy-slider.jsx
@@ -64,7 +64,7 @@ export default class ReactSlidySlider extends Component {
   }
 
   componentWillUnmount () {
-    this.slidyInstance.destroy()
+    this.slidyInstance && this.slidyInstance.destroy()
   }
 
   shouldComponentUpdate (nextProps, {currentSlide}) {
