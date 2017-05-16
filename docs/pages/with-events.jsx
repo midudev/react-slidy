90df5fc838d70c7ea9ffe6e9594afeff60899f1f
Miguel Ángel Durán <miguel@miduga.es>
2017-05-16 22:03:45 +0100

    Add new with-events example

---
+++ docs/pages/with-events.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import ReactSlidy from '../../src'
+
+import { DisplayCode } from '../utils'
+
+export const demo = (
+  <div className='text-center'>
+    <p>You can use doBeforeSlide and doAfterSlide props to execute some code on those important events.</p>
+    <ReactSlidy
+      doAfterSlide={({currentSlide}) => {
+        document.getElementById('demo-with-events').innerHTML += `
+        doAfterSlide currentSlide: ${currentSlide}<br />`
+      }}
+      doBeforeSlide={({currentSlide, nextSlide}) => {
+        document.getElementById('demo-with-events').innerHTML += `
+        doBeforeSlide currentSlide: ${currentSlide} nextSlide: ${nextSlide}<br />`
+      }}
+      infinite={false}
+      slidesToScroll={1}>
+      <a href='http://miduga.es'>
+        <img src='../img/beautiful_landscapes.jpg' />
+      </a>
+      <a href='https://github.com/miduga/react-slidy'>
+        <img src='../img/beautiful_landscapes_02.jpg' />
+      </a>
+    </ReactSlidy>
+    <p id='demo-with-events' />
+    <DisplayCode>
+      {`<ReactSlidy
+      infinite={false}
+      slidesToScroll={1}>
+      <a href='http://miduga.es'>
+        <img src='../img/beautiful_landscapes.jpg' />
+      </a>
+      <a href='https://github.com/miduga/react-slidy'>
+        <img src='../img/beautiful_landscapes_02.jpg' />
+      </a>
+    </ReactSlidy>`
+      }
+    </DisplayCode>
+  </div>
+)
