5b008d9658cca96babb9b9e1e69206774aa8721d
Miguel Ángel Durán <miguel@miduga.es>
2017-06-29 10:57:58 +0200

    Use new lazy-load package slimmer

--- src/react-slidy.jsx
+++ src/react-slidy.jsx
@@ -1,7 +1,7 @@
 import React, { Component, PropTypes as T } from 'react'
 import ReactSlidySlider from './react-slidy-slider'
 import Spinner from '@schibstedspain/sui-spinner'
-import LazyLoad from 'react-lazy-load'
+import LazyLoad from 'react-lazyload'
 
 const spinnerConfig = {
   size: 20,
@@ -55,29 +55,19 @@ ReactSlidy.propTypes = {
   ]).isRequired,
   classNameBase: T.string,
   lazyLoadSlider: T.bool,
-  // Shape from https://github.com/loktar00/react-lazy-load/blob/master/src/LazyLoad.jsx#L121
+  // Shape from https://github.com/jasonslyvia/react-lazyload/blob/master/src/index.jsx#L295
   lazyLoadConfig: T.shape({
-    className: T.string,
-    debounce: T.bool,
-    elementType: T.string,
+    /* In the first round of render, LazyLoad will render a placeholder for your component if no placeholder is provided and measure if this component is visible. Set height properly will make LazyLoad calculate more precisely. The value can be number or string like '100%'. You can also use css to set the height of the placeholder instead of using height. */
     height: T.oneOfType([
       T.string,
       T.number
     ]),
+    /* Say if you want to preload a component even if it's 100px below the viewport (user have to scroll 100px more to see this component), you can set offset props to 100. On the other hand, if you want to delay loading a component even if it's top edge has already appeared at viewport, set offset to negative number. */
     offset: T.number,
-    offsetBottom: T.number,
-    offsetHorizontal: T.number,
-    offsetLeft: T.number,
-    offsetRight: T.number,
-    offsetTop: T.number,
-    offsetVertical: T.number,
-    threshold: T.number,
-    throttle: T.number,
-    width: T.oneOfType([
-      T.string,
-      T.number
-    ]),
-    onContentVisible: T.func
+    /* Once the lazy loaded component is loaded, do not detect scroll/resize event anymore. Useful for images or simple components. */
+    once: T.bool,
+    /* If lazy loading components inside a overflow container, set this to true. Also make sure a position property other than static has been set to your overflow container. */
+    overflow: T.bool
   }),
   tailArrowClass: T.string
 }
@@ -86,7 +76,9 @@ ReactSlidy.defaultProps = {
   classNameBase: 'react-Slidy',
   lazyLoadSlider: true,
   lazyLoadConfig: {
-    offsetVertical: 500
+    height: 150,
+    once: true,
+    offset: 150
   },
   tailArrowClass: 'react-Slidy-arrow--disabled'
 }
