64b32cfe58c27518e0a13fe50215b4b526bea2ac
Miguel Ángel Durán <miguel@miduga.es>
2017-08-01 09:01:41 +0200

    Improve prefix detection

--- src/slidy/detect-prefixes.js
+++ src/slidy/detect-prefixes.js
@@ -1,58 +1,46 @@
-const TRANSLATE_3D_INIT = 'translate3d(0, 0, 0)'
+const EMPTY_STRING = ''
 
 // Detect prefixes for saving time and bytes
 export default function detectPrefixes () {
   let transform
   let transition
   let transitionEnd
-  let translate
 
-  (function () {
-    const el = document.createElement('div')
-    const style = el.style
-    let prop
+  const style = document.createElement('div').style
+  let prop
 
-    if (style[prop = 'webkitTransition'] === '') {
-      transitionEnd = 'webkitTransitionEnd'
-      transition = prop
-    }
+  if (style[prop = 'webkitTransition'] === EMPTY_STRING) {
+    transitionEnd = 'webkitTransitionEnd'
+    transition = prop
+  }
 
-    if (style[prop = 'transition'] === '') {
-      transitionEnd = 'transitionend'
-      transition = prop
-    }
+  if (style[prop = 'transition'] === EMPTY_STRING) {
+    transitionEnd = 'transitionend'
+    transition = prop
+  }
 
-    if (style[prop = 'webkitTransform'] === '') {
-      transform = '-webkit-transform'
-    }
+  if (style[prop = 'webkitTransform'] === EMPTY_STRING) {
+    transform = '-webkit-transform'
+  }
 
-    if (style[prop = 'msTransform'] === '') {
-      transform = '-ms-transform'
-    }
+  if (style[prop = 'msTransform'] === EMPTY_STRING) {
+    transform = '-ms-transform'
+  }
 
-    if (style[prop = 'transform'] === '') {
-      transform = prop
-    }
+  if (style[prop = 'transform'] === EMPTY_STRING) {
+    transform = prop
+  }
 
-    document.body.insertBefore(el, null)
-    style[transform] = TRANSLATE_3D_INIT
-    const hasTranslate3d = !!el.style.getPropertyValue(transform)
-
-    translate = (function () {
-      return hasTranslate3d
-        ? function (to) { return 'translate3d(' + to + 'px, 0, 0)' }
-        : function (to) { return 'translate(' + to + 'px, 0)' }
-    }())
-
-    document.body.removeChild(el)
-  }())
+  const translate = typeof style.perspective !== 'undefined'
+                      ? function (to) { return 'translate3d(' + to + 'px, 0, 0)' }
+                      : function (to) { return 'translate(' + to + 'px, 0)' }
 
   return {
     transform,
-    translate,
     transition,
     transitionEnd,
-    transitionTiming: transition + '-timing-function',
-    transitionDuration: transition + '-duration'
+    translate,
+    transitionDuration: transition + '-duration',
+    transitionTiming: transition + '-timing-function'
   }
 }
