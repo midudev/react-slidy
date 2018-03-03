0a2cad09348b59def04c67502edd53f32752f110
Miguel Angel Duran Garcia <miduga@gmail.com>
2018-03-03 17:52:28 +0100

    Rewrite of the slider, keep as much as possible on React

--- src/index.js
+++ src/index.js
@@ -1,18 +1,38 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import ReactSlidySlider from './react-slidy-slider'
-import Spinner from '@schibstedspain/sui-spinner-basic'
+import debounce from 'just-debounce-it'
 
+const NO_OP = () => {}
+
+let moveScroll = NO_OP
 // if window is present, then we get the needed library
 if (typeof window !== 'undefined' && window.document) {
   require('intersection-observer')
+  moveScroll = require('./move').moveScroll
 }
 
 export default class ReactSlidy extends Component {
   constructor (props) {
     super(props)
     this.observer = null
-    this.state = { showSlider: !this.props.lazyLoadSlider }
+    this.state = {
+      showSlider: !this.props.lazyLoadSlider
+    }
+
+    this._currentIndex = 0
+    this._frameDOMEl = undefined
+    this._sliderDOMEl = undefined
+    this._sliderWidth = 0
+    this._slides = React.Children.toArray(this.props.children)
+
+    this._getSliderWidth = this._getSliderWidth.bind(this)
+    this._handleIntersection = this._handleIntersection.bind(this)
+    this._handleNext = this._handleNext.bind(this)
+    this._handlePrev = this._handlePrev.bind(this)
+    this._handleResize = debounce(this._handleResize.bind(this), 200)
+    this._handleSlide = this._handleSlide.bind(this)
+    this._handleTouchEnd = this._handleTouchEnd.bind(this)
+    this._renderItem = this._renderItem.bind(this)
   }
 
   componentDidMount () {
@@ -26,8 +46,10 @@ export default class ReactSlidy extends Component {
       this.observer = new window.IntersectionObserver(this._handleIntersection, {
         rootMargin: `${offset}px 0px 0px`
       })
-      this.observer.observe(this.node)
+      this.observer.observe(this._sliderDOMEl)
     }
+
+    window.addEventListener('resize', this._handleResize)
   }
 
   componentDidCatch (error, errorInfo) {
@@ -38,38 +60,113 @@ export default class ReactSlidy extends Component {
   // to add new childrens to it, so we don't want unexpected behaviour
   // expect if we specify we have dynamicContent on it
   shouldComponentUpdate (nextProps, nextState) {
-    return this.props.dynamicContent || nextState.showSlider !== this.state.showSlider
+    return true
+    // return this.props.dynamicContent || nextState.showSlider !== this.state.showSlider
   }
 
   componentWillUnmount () {
     this.observer && this.observer.disconnect()
-  }
-  renderSlider () {
-    return (
-      <ReactSlidySlider {...this.props}>
-        {this.props.children}
-      </ReactSlidySlider>
-    )
+    window.removeEventListener('resize', this._handleResize)
   }
 
-  _handleIntersection = ([entry], observer) => {
+  _getSliderWidth ({ force = false }) {
+    if (this._sliderWidth === 0 || force) {
+      this._sliderWidth = this._sliderDOMEl.clientWidth
+    }
+  }
+
+  _handleIntersection ([entry], observer) {
     if (entry.isIntersecting || entry.intersectionRatio > 0) {
       observer.unobserve(entry.target)
       this.setState({ showSlider: true })
     }
   }
 
+  _handleNext (e) {
+    e.preventDefault()
+    this._handleSlide({ direction: 1 })
+  }
+
+  _handlePrev (e) {
+    e.preventDefault()
+    this._handleSlide({ direction: -1 })
+  }
+
+  _handleTouchEnd (e) {
+    const scrollLeft = this._frameDOMEl.scrollLeft
+    const actualPosition = this._currentIndex * this._sliderWidth
+    const direction = actualPosition < scrollLeft ? 1 : -1
+
+    const pointForMovement = actualPosition + ((this._sliderWidth) / 3 * direction)
+    const newIndex = this._currentIndex + direction
+    const newPosition = actualPosition + (this._sliderWidth * direction)
+
+    let left = actualPosition
+
+    if (
+      (direction === 1 && scrollLeft > pointForMovement) ||
+      (direction === -1 && scrollLeft < pointForMovement)
+    ) {
+      left = newPosition
+      this._currentIndex = newIndex
+    }
+
+    moveScroll(this._frameDOMEl, left)
+  }
+
+  _handleSlide ({direction}) {
+    const newIndex = this._currentIndex + direction
+    if (newIndex >= this._slides.length || newIndex < 0) return
+
+    const left = newIndex * this._sliderWidth
+    moveScroll(this._frameDOMEl, left)
+    this._currentIndex = newIndex
+  }
+
+  _handleResize () {
+    this._getSliderWidth({ force: true })
+    moveScroll(this._frameDOMEl, this._currentIndex * this._sliderWidth)
+  }
+
+  _renderItem (item, index) {
+    const { classNameBase } = this.props
+    return (
+      <li
+        className={`${classNameBase}-slide`}
+        key={`${this.dynamicContentIndex}${index}`}>
+        {item}
+      </li>
+    )
+  }
+
+  _renderItems () {
+    const { initialSlide, itemsToPreload } = this.props
+    // return slides.slice(0, initialSlide + itemsToPreload).map(this._renderItem)
+    return this._slides.map(this._renderItem)
+  }
+
   render () {
     const { showSlider } = this.state
+    const { classNameBase, showArrows } = this.props
 
     return (
       <div
         className={this.props.classNameBase}
-        ref={node => { this.node = node }}>
-        <Spinner size='medium' />
-        {
-          showSlider && this.renderSlider()
+        onMouseEnter={this._getSliderWidth}
+        onTouchEnd={this._handleTouchEnd}
+        onTouchStart={this._getSliderWidth}
+        ref={node => { this._sliderDOMEl = node }}>
+        {showArrows &&
+          <button className={`${classNameBase}-prev`} onClick={this._handlePrev} />
         }
+        {showArrows &&
+          <button className={`${classNameBase}-next`} onClick={this._handleNext} />
+        }
+        <ul
+          className={`${classNameBase}-frame`}
+          ref={node => { this._frameDOMEl = node }}>
+          {this._renderItems()}
+        </ul>
       </div>
     )
   }
@@ -81,7 +178,11 @@ ReactSlidy.propTypes = {
     PropTypes.object
   ]).isRequired,
   classNameBase: PropTypes.string,
-  dynamicContent: PropTypes.bool,
+
+  doAfterDestroy: PropTypes.func,
+  doAfterSlide: PropTypes.func,
+  doBeforeSlide: PropTypes.func,
+
   lazyLoadSlider: PropTypes.bool,
   // Shape from https://github.com/jasonslyvia/react-lazyload/blob/master/src/index.jsx#L295
   lazyLoadConfig: PropTypes.shape({
@@ -90,15 +191,21 @@ ReactSlidy.propTypes = {
     /* If lazy loading components inside a overflow container, set this to true. Also make sure a position property other than static has been set to your overflow container. */
     overflow: PropTypes.bool
   }),
+  showArrows: PropTypes.bool,
   tailArrowClass: PropTypes.string
 }
 
 ReactSlidy.defaultProps = {
   classNameBase: 'react-Slidy',
-  dynamicContent: false,
+
+  doAfterDestroy: NO_OP,
+  doAfterSlide: NO_OP,
+  doBeforeSlide: NO_OP,
+
   lazyLoadSlider: true,
   lazyLoadConfig: {
     offset: 150
   },
+  showArrows: true,
   tailArrowClass: 'react-Slidy-arrow--disabled'
 }
