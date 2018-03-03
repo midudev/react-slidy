f8d7c870be9d7904d1a56ea6d30dfac2820a31a7
Miguel Angel Duran Garcia <miduga@gmail.com>
2018-03-03 17:50:45 +0100

    Rename files and move to new propTypes

--- docs/App.js
+++ docs/App.js
@@ -7,28 +7,19 @@ import 'highlight.js/styles/monokai-sublime.css'
 // Then register them with lowlight
 Lowlight.registerLanguage('js', js)
 
-const pages = [
-  'intro',
-  'with-initial-index',
-  'with-other-components',
-  'with-links',
-  'with-events',
-  'with-dynamic-content'
-]
+import pages from './pages/*.jsx';
 
 const navButtonsContent = `
 <a class="github-button" href="https://github.com/miduga/react-slidy" aria-label="React Slidy Repo">Repo</a>
-<a class="github-button" href="https://github.com/miduga/react-slidy" data-icon="octicon-star" data-count-href="/miduga/react-slidy/stargazers" data-count-api="/repos/miduga/react-slidy#stargazers_count" data-count-aria-label="# stargazers on GitHub" aria-label="Star miduga/react-slidy on GitHub">Star</a>
-<a class="github-button" href="https://github.com/miduga/react-slidy/issues" data-icon="octicon-issue-opened" data-count-api="/repos/miduga/react-slidy#open_issues_count" data-count-aria-label="# issues on GitHub" aria-label="Issue miduga/react-slidy on GitHub">Issue</a>
+<a class="github-button" href="https://github.com/miduga/react-slidy" data-icon="octicon-star" data-count-href="/miduga/react-slidy/stargazers" data-show-count="true" data-count-aria-label="# stargazers on GitHub" aria-label="Star miduga/react-slidy on GitHub">Star</a>
+<a class="github-button" href="https://github.com/miduga/react-slidy/issues" data-icon="octicon-issue-opened" data-show-count="true" data-count-aria-label="# issues on GitHub" aria-label="Issue miduga/react-slidy on GitHub">Issue</a>
 `
 
-const pagesComponents = pages.reduce((acc, page) => {
-  acc[page] = require(`./pages/${page}.jsx`).demo
-  return acc
-}, {})
-
 export default class App extends Component {
-  state = { activeSection: pages[0] }
+  constructor (...args) {
+    super(...args)
+    this.state = { activeSection: pages[0] }
+  }
 
   componentDidMount () {
     const hash = window.location.hash
@@ -60,7 +51,7 @@ export default class App extends Component {
             className='nav-buttons mt1 mb2'
             dangerouslySetInnerHTML={{ __html: navButtonsContent }}
           />
-          {pages.map(demo => (
+          {Object.keys(pages).map(demo => (
             <a
               href={`#${demo}`}
               className={activeSection === demo ? 'active' : ''}
@@ -73,9 +64,9 @@ export default class App extends Component {
         </nav>
 
         <section className='content'>
-          {pages.map(demo => (
+          {Object.keys(pages).map(demo => (
             <div key={demo} id={demo}>
-              {pagesComponents[demo]}
+              {pages[demo].demo}
             </div>
           ))}
         </section>
--- docs/Tag.js
+++ docs/Tag.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
 
 export default function Tag ({text, inverted = false}) {
   return (
