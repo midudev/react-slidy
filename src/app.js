8835a4bdd22972cad6cf8bb5a97ffab37b073ae4
Miguel Angel Duran Garcia <miduga@gmail.com>
2019-03-09 16:24:21 +0100

    Fix now build because case sensitive

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Demo from './demo/demo'
+import Demo from './demo/Demo'
 import './demo/index.scss'
 
 ReactDOM.render(<Demo />, document.getElementById('root'))
