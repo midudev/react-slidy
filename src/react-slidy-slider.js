42d8ed36fe791dad6f1b0ad5636d904a9ce5ffe2
Miguel Angel Duran Garcia <miduga@gmail.com>
2019-03-11 15:57:47 +0100

    Rewrite react-slidy-slider

--- src/react-slidy-slider.js
+++ src/react-slidy-slider.js
@@ -1,46 +1,60 @@
-import React, {useEffect, useRef, useState} from 'react'
-import PropTypes from 'prop-types'
-
-import slidy from './slidy/slidy'
-
+/* eslint-disable react/prop-types */
+import React, {Fragment, useEffect, useRef, useState} from 'react'
+import slidy from './slidy'
 function noop() {}
-
-function getClassesName(base) {
-  return {
-    frame: `${base}-frame`,
-    item: `${base}-item`,
-    next: `${base}-next`,
-    prev: `${base}-prev`,
-    slides: `${base}-slides`
-  }
-}
 
 function convertToArrayFrom(children) {
   return Array.isArray(children) ? children : [children]
 }
 
-export default function ReactSlidySlider(props) {
+function getItemsToRender(index, maxIndex, items, itemsToPreload) {
+  const preload = index > 0 ? itemsToPreload + 1 : itemsToPreload
+  return items.slice(0, maxIndex + preload)
+}
+
+function destroySlider(slidyInstance, doAfterDestroy) {
+  slidyInstance && slidyInstance.clean() && slidyInstance.destroy()
+  doAfterDestroy()
+}
+
+function renderItem(item, index) {
+  return <li key={index}>{item}</li>
+}
+
+export default function ReactSlidySlider({
+  classNameBase,
+  children,
+  ease,
+  doAfterDestroy,
+  doAfterSlide,
+  doBeforeSlide,
+  initialSlide,
+  itemsToPreload,
+  slideSpeed,
+  showArrows,
+  tailArrowClass
+}) {
   const [slidyInstance, setSlidyInstance] = useState({
     next: noop,
     prev: noop,
     updateItems: noop
   })
-  const [index, setIndex] = useState(props.initialSlide)
-  const [maxIndex, setMaxIndex] = useState(props.initialSlide)
-  const sliderDOMEl = useRef(null)
+  const [index, setIndex] = useState(initialSlide)
+  const [maxIndex, setMaxIndex] = useState(initialSlide)
+  const sliderContainerDOMEl = useRef(null)
+  const slidesDOMEl = useRef(null)
 
-  const classes = getClassesName(props.classNameBase)
-  let items = convertToArrayFrom(props.children)
-
-  const destroySlider = () => {
-    slidyInstance && slidyInstance.clean() && slidyInstance.destroy()
-    props.doAfterDestroy()
-  }
+  const items = convertToArrayFrom(children)
 
   useEffect(function() {
-    const slidyInstance = slidy(sliderDOMEl.current, {
-      classNameSlideContainer: classes.slides,
-      ...props,
+    const slidyInstance = slidy(sliderContainerDOMEl.current, {
+      ease,
+      doAfterSlide,
+      doBeforeSlide,
+      slideSpeed,
+      slidesDOMEl: slidesDOMEl.current,
+      initialSlide: index,
+      items: items.length,
       onNext: nextIndex => {
         setIndex(nextIndex)
         nextIndex > maxIndex && setMaxIndex(nextIndex)
@@ -49,80 +63,33 @@ export default function ReactSlidySlider(props) {
       onPrev: nextIndex => {
         setIndex(nextIndex)
         return nextIndex
-      },
-      initialSlide: index,
-      items: items.length,
-      frameDOMEl: sliderDOMEl.current
+      }
     })
+
     setSlidyInstance(slidyInstance)
-    return () => destroySlider()
+    return () => destroySlider(slidyInstance, doAfterDestroy)
   }, [])
 
   useEffect(function() {
     slidyInstance && slidyInstance.updateItems(items.length)
   })
 
-  const renderItem = (item, index) => {
-    return (
-      <li className={classes.item} key={index}>
-        {item}
-      </li>
-    )
-  }
-
-  const renderItems = () => {
-    return items.slice(0, maxIndex + props.itemsToPreload).map(renderItem)
-  }
-
-  const {showArrows, tailArrowClass} = props
-  const prevArrowClassName = `${classes.prev} ${
-    index === 0 ? tailArrowClass : ''
-  }`
-  const nextArrowClassName = `${classes.next} ${
-    index === items.length - 1 ? tailArrowClass : ''
-  }`
+  const itemsToRender = getItemsToRender(index, maxIndex, items, itemsToPreload)
 
   return (
-    <div className={classes.frame} ref={sliderDOMEl}>
+    <Fragment>
       {showArrows && (
-        <span className={prevArrowClassName} onClick={slidyInstance.prev} />
+        <Fragment>
+          <span disabled={index === 0} onClick={slidyInstance.prev} />
+          <span
+            disabled={index === items.length - 1}
+            onClick={slidyInstance.next}
+          />
+        </Fragment>
       )}
-      {showArrows && (
-        <span className={nextArrowClassName} onClick={slidyInstance.next} />
-      )}
-      <ul className={classes.slides}>{renderItems()}</ul>
-    </div>
+      <div ref={sliderContainerDOMEl}>
+        <ul ref={slidesDOMEl}>{itemsToRender.map(renderItem)}</ul>
+      </div>
+    </Fragment>
   )
-}
-
-ReactSlidySlider.propTypes = {
-  children: PropTypes.oneOfType([PropTypes.array, PropTypes.object]).isRequired,
-  classNameBase: PropTypes.string,
-  dynamicContent: PropTypes.bool,
-  doAfterDestroy: PropTypes.func,
-  doAfterSlide: PropTypes.func,
-  doBeforeSlide: PropTypes.func,
-  ease: PropTypes.string,
-  initialSlide: PropTypes.number,
-  itemsToPreload: PropTypes.number,
-  onReady: PropTypes.func,
-  rewindSpeed: PropTypes.number,
-  showArrows: PropTypes.bool,
-  slideSpeed: PropTypes.number,
-  snapBackSpeed: PropTypes.number,
-  tailArrowClass: PropTypes.string
-}
-
-ReactSlidySlider.defaultProps = {
-  doAfterDestroy: noop,
-  doAfterSlide: noop,
-  doBeforeSlide: noop,
-  ease: 'ease',
-  initialSlide: 0,
-  itemsToPreload: 1,
-  onReady: noop,
-  rewindSpeed: 500,
-  showArrows: true,
-  slideSpeed: 500,
-  snapBackSpeed: 500
 }
