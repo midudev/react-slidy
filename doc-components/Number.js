1f10f392b9620eb06284bfb0b88845762a32a9f4
midudev <miduga@gmail.com>
2020-01-17 20:49:42 +0100

    Improve docs

--- doc-components/Number.js
+++ doc-components/Number.js
@@ -1,21 +1,18 @@
 import React from 'react'
 
+const style = {
+  alignContent: 'center',
+  alignItems: 'center',
+  backgroundColor: '#eee',
+  color: '#888',
+  display: 'flex',
+  fontSize: '100px',
+  height: '250px',
+  justifyContent: 'center',
+  textShadow: '2px 2px 0px #aaa',
+  width: '100%'
+}
+
 export default ({num}) => { // eslint-disable-line
-  return (
-    <span
-      style={{
-        color: '#888',
-        height: '250px',
-        fontSize: '100px',
-        display: 'flex',
-        alignItems: 'center',
-        alignContent: 'center',
-        justifyContent: 'center',
-        textShadow: '2px 2px 0px #aaa',
-        width: '100%'
-      }}
-    >
-      {num}
-    </span>
-  )
+  return <span style={style}>{num}</span>
 }
